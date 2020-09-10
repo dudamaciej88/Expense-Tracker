fd287609540c17e20f72ad016a55cfa8d2bdc328
dudamaciej88 <dudamaciej88@gmail.com>
2020-09-10 20:30:42 +0200

    Add GlobalProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Header from "./components/Header/Header";
+import Balance from "./components/Balance/Balance";
+import IncomeExpenses from "./components/IncomeExpenses/IncomeExpenses";
+import TransactionList from "./components/TransactionList/TransactionList";
+import AddTransaction from "./components/AddTransaction/AddTransaction";
+import { GlobalStyle } from "./App.style";
+
+import { GlobalProvider } from "./context/GlobalState";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <GlobalProvider>
+      <GlobalStyle />
+      <div>
+        <Header />
+        <div className="container">
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+        </div>
+      </div>
+    </GlobalProvider>
+  );
 };
 
 export default App;
